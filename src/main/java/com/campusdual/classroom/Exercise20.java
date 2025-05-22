b8255dc5586014ceed27e8f2538787d8e06bd19d
VictorLopezGarcia <victor.lopezg@udc.es>
2025-05-22 13:09:11 +0200

    Ejercicio20

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -6,11 +6,18 @@ import java.util.List;
 public class Exercise20 {
 
     public static List<Person> getPeopleList() {
-
+        List<Person> list = new ArrayList<>();
+        list.add(new Person("John", "Smith"));
+        list.add(new Teacher("María", "Montessori", "Educación"));
+        list.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
+        list.add(new Doctor("Gregory", "House", "Nefrología e infectología"));
+        return list;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
-
+        for (Person person : stringList) {
+            person.getDetails();
+        }
     }
 
     public static void main(String[] args) {
